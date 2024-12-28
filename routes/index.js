76d0380ac4ae26f2144b177fd624a0448addd1f1
CStras <cstrasserGH@gmail.com>
2024-12-27 22:29:42 -0600

    chore: linter error fix

--- routes/index.js
+++ routes/index.js
@@ -1,8 +1,7 @@
-const { NOT_FOUND_STATUS } = require("../utils/errors");
-
 const router = require("express").Router();
 const userRouter = require("./users");
 const itemRouter = require("./clothingItems");
+const { NOT_FOUND_STATUS } = require("../utils/errors");
 
 router.use("/users", userRouter);
 router.use("/items", itemRouter);
